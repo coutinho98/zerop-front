ae764f56e74fb4267dceffff3d67be8cc672dbf9
coutinho98 <mateuscouto98@outlook.com>
2026-01-14 04:59:34 -0300

    feat: added /signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
